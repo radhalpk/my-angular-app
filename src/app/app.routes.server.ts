a23ff642310d70db853b0393117d8960b519537f
Pavan Kumar Radhala <pavankumarradhala@Pavans-MacBook-Pro.local>
2025-03-12 07:10:52 -0400

    javascript

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,38 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { RenderMode } from '@angular/ssr'; // Import RenderMode
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent,
+    
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    
+  },
+  { 
+    path: 'contact', 
+    component: ContactComponent,
+    
+  },
+  { 
+    path: 'about', 
+    component: AboutComponent,
+    
+  },
 ];
+
+// Export the routes as serverRoutes
+export const serverRoutes = routes;
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
